09561f442b2239f19ef8f2f875306baa08917ad0
SHIVAM ARORA <230753894+Only1Dev@users.noreply.github.com>
2026-01-27 19:23:50 +0530

    Add files via upload

---
+++ api.js
@@ -0,0 +1,14 @@
+export default async function handler(req, res) {
+  try {
+    const url = "https://api.openaq.org/v2/latest?city=Delhi";
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.status(200).json(data);
+
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch AQI data" });
+  }
+}
