d87d42070c6a5bdf613b9f97d2a881eb4e178b95
SHIVAM ARORA <230753894+Only1Dev@users.noreply.github.com>
2026-01-27 19:58:34 +0530

    Change AQI API endpoint and add API key
    Updated API endpoint to v3 and added API key in headers.

--- api/aqi.js
+++ api/aqi.js
@@ -4,11 +4,19 @@ export const config = {
 
 export default async function handler() {
   const response = await fetch(
-    "https://api.openaq.org/v2/latest?city=Delhi"
+    "https://api.openaq.org/v3/latest?city=Delhi&limit=1",
+    {
+      headers: {
+        "X-API-Key": "7a5a924eaabd15ed6f1545e43255f89ae61a0b7fdc96ca770715da7b64515fc7"
+      }
+    }
   );
+
   const data = await response.json();
 
   return new Response(JSON.stringify(data), {
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json"
+    }
   });
 }
